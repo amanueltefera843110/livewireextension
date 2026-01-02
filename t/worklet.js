39e57fb414dd5b71543919e8ca0dd75240bd56ff
amanueltefera843110 <amanuel.tefera@hope.edu>
2026-01-02 12:32:52 -0500

    Delete t directory

--- t/worklet.js
+++
@@ -1,8 +0,0 @@
-class RecorderProcessor extends AudioWorkletProcessor {
-  process(inputs) {
-    const input = inputs[0][0];
-    if (input) this.port.postMessage(input);
-    return true;
-  }
-}
-registerProcessor("recorder-processor", RecorderProcessor);
